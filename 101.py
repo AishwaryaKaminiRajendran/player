db1da520704230386b0b7dc8c416603a2a185513
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-05-13 12:26:46 +0530

    Create 101.py

---
+++ 101.py
@@ -0,0 +1,10 @@
+a = int (input ())
+s = 0 
+b =[int (a) for a in input ().split ()]
+for i in range (0, len (b)-1):
+    j=i+1
+    if b[i]>b[j]:
+       s=s+b[i]
+    else:
+        s=s+b[j]
+print(s)
