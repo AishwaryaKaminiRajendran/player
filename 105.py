2a136142bb23197959cc0ab4a43f07f23bed94b7
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-05-13 14:07:40 +0530

    Create 105.py

---
+++ 105.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=[int(a) for a in input().split()]
+c=sorted(b)
+#print(c)
+d=[]
+for i in range (0,len(b)):
+    for j in range(0,len(c)):
+        if b[i]==c[j]:
+            print(j+1,end=" ")
