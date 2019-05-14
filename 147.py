b0c26a409df1b7e1c342d054e2ffdbd76484e661
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-05-14 03:42:57 -0700

    Update 147.py

--- 147.py
+++ 147.py
@@ -1,7 +1,9 @@
 s=input().split()
-
-print(s[0],end=" ")
-
-for i in range(1,len(s)-1):
-    print(s[i][::-1],end=" ")
-print(s[-1])
+if len(s)==1 or len(s)==2:
+     for i in range(0,len(s)):
+        print(s[i],end=" ")
+else:
+    print(s[0],end=" ")
+    for i in range(1,len(s)-1):
+        print(s[i][::-1],end=" ")
+    print(s[-1])
