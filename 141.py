d4ace4ae9532484230e0e6cd150f1d85e624b309
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-05-13 23:36:46 -0700

    Create 141.py

---
+++ 141.py
@@ -0,0 +1,15 @@
+n=int(input())
+b=[]
+c=0
+for i in range(0,n):
+    a=input()
+    b.append(a)
+print(b)
+for x in range(0,len(b)-1):
+    y=x+1
+    if b[x]==b[y]:
+       c=c+1
+if c>=1:
+    print("yes")
+else:
+    print("no")
