af582ffbd77aee4f168f81df53c77715fb4bd241
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-08-08 07:14:04 -0700

    Update 23.py

--- 23.py
+++ 23.py
@@ -1,6 +1,6 @@
 n,k=map(int,input().split())
-a=[int(n) for n in input().split() ]
+x=[int(n) for n in input().split() ]
 b=[int(k) for k in input().split() ]  
 for i in range(0,len(b)):
-    a.append(b[i])
-    print(max(a),end=" ")
+    x.append(b[i])
+    print(max(x),end=" ")
