ad644d919e30fefe3c020163c5312eebed35ab31
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-08-08 07:25:56 -0700

    Update 27.py

--- 27.py
+++ 27.py
@@ -1,6 +1,6 @@
-n=input()
-for i in range(0,len(n)):
-    if n[i].isupper():
-        print(n[i].lower(),end="")
+a=input()
+for i in range(0,len(a)):
+    if a[i].isupper():
+        print(a[i].lower(),end="")
     else:
-        print(n[i].upper(),end="")
+        print(a[i].upper(),end="")
